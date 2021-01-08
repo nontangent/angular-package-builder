c68f70d84202be5b7e61631952cbffa8618983d5
nontangent <irregular0number@gmail.com>
2021-01-08 10:19:01 +0900

    feat: resolve host css variable

--- src/ng-packagr/stylesheet-processor.ts
+++ src/ng-packagr/stylesheet-processor.ts
@@ -1,7 +1,9 @@
 import { StylesheetProcessor as _StylesheetProcessor } from 'ng-packagr/lib/styles/stylesheet-processor';
+import * as functionsResolver from 'calc-loader/functions-resolver';
 
 export class StylesheetProcessor extends _StylesheetProcessor {
   process(filePath: string, content: string) {
+    content = functionsResolver(content, ['hvar']);
     return super.process(filePath, content);
   }
 }
